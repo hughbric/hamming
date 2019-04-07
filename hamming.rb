d6c1a1eafcd8d626e094875eb9b859f9c7018e2f
Michael Hughes <mhughes36@live.co.uk>
2019-04-07 20:34:39 +0100

    Refactored

--- hamming.rb
+++ hamming.rb
@@ -2,11 +2,9 @@ class Hamming
   def self.compute(strand, comparison)    
     raise ArgumentError unless strand.length == comparison.length
     
-    return 0 if strand == comparison
-
-    c_bytes = comparison.bytes
-    strand.each_byte.with_index.count do |item, index|
-      item != c_bytes[index]
-    end    
+    c_bytes = comparison.each_byte
+    strand.each_byte.count do |item|
+      item != c_bytes.next
+    end
   end
 end
